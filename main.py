90575f8539ad7e0566ba35923b344c716b3183fa
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2013-01-26 03:19:41 -0500

    Dear lazyweb, I hate pylast.  Love, Corgi.

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Enya - a crappy IRC bot that spews NP info
 # Copyright (C) 2013 Elizabeth Myers. Licensed under the WTFPL.
 
 from gevent import socket, monkey, sleep, spawn, joinall
 monkey.patch_all()
 
-import pylast
 from collections import namedtuple
 from copy import deepcopy
+from settings import server,port,nick,user,realname,channels
+import urllib.request as urlreq
+import xml.dom.minidom as minidom
 
 # User configurable parts
-USERNAME = "changeme"
-PASSWORD = "changeme" # pylast.md5(string)
-APIKEY = "get from http://last.fm/api"
-SECRET = "get from http://last.fm/api"
+USERNAME = "therealelizacat"
+PASSWORD = "499c2d7ae9b60028c286c6b20f8f3d81" # pylast.md5() - hint.
+APIKEY = "bf68f12315aea1b0b08b782cd5f59ad9"
+SECRET = "22fea42e3a87dd0d4a0b44cd5bf156b5"
 
-server = "irc.interlinked.me"
-port = 6667
-nick = user = "Enya"
-realname = "Your worst nightmare."
-channels = ["#enya"]
-
+admin_nicks = ['Elizacat',]
+admin_hosts = ['always.lowing.the.bar',]
 # None below here
 
 # TODO: make these not global -.-
-with open('userlist.txt', 'r') as file:
-    userlist = [x.rstrip('\n') for x in file]
+user_changed = False
 
-npcache = {}
+def maybe_add_to_userlist(irc, admin, user):
+    global user_changed
+    userlist = load_users()
+    if param in userlist:
+        irc.send(self.parsed_line(None, "PRIVMSG", [admin, 'Already in there.']))
+        return
+    
+    userlist.append(param)
+    
+    with open('userlist.txt', 'w') as f:
+        for l in userlist:
+            f.write('\n'.join(userlist))
+
+    irc.send(self.parsed_line(None, "PRIVMSG", [admin, 'Done!']))
+    user_changed = True
+
+def maybe_del_from_userlist(irc, admin, user):
+    global user_changed
+    userlist = load_users()
+    if param not in userlist:
+        self.send(self.parsed_line(None, "PRIVMSG", [admin,  'Not in there.']))
+        return
+    
+    userlist.remove(param)
+    
+    with open('userlist.txt', 'w') as f:
+        for l in userlist:
+            f.write('\n'.join(userlist))
+    
+    irc.send(self.parsed_line(None, "PRIVMSG", [admin, 'Done!']))
+    user_changed = True
 
 class IRC:
     # TODO - WOAH FIX THIS PIECE OF SHIT
@@ -112,57 +139,31 @@ class IRC:
             x, sep, y = line.hostmask.partition('@')
             if sep is None or y is None: return
 
-            # FIXME - configurable auth...
-            # Change 'this to your host
-            if y != 'always.lowering.the.bar': return
-
+            if y not in admin_hosts: return
             x, sep, y = x.partition('!')
-            if not x.startswith('Elizacat'): return # Change to your nick
+            if True not in [x.startswith(nick) for nick in admin_nicks]: return # XXX this is disgusting
 
             msg = line.params[-1]
             cmd, sep, param = msg.partition(' ')
             cmd = cmd.lower()
+
+            if not cmd:
+                print("Empty message ", msg)
+                return
 
             if cmd[0] != '!':
                 return
             else:
                 cmd = cmd[1:]
 
-            # Change these nicks below too etc.
             if cmd == 'add':
-                if param in userlist:
-                    self.send(self.parsed_line(None, "PRIVMSG", ['Elizacat', 'Already in there.']))
-                    return
-
-                userlist.append(param)
-                npcache[param] = ((None,)*5, None)
-
-                with open('userlist.txt', 'w') as file:
-                    for l in userlist:
-                        file.write(l)
-                        file.write('\n')
-
-                self.send(self.parsed_line(None, "PRIVMSG", ['Elizacat', 'Done!']))
+                maybe_add_to_userlist(self, x, param)
 
             elif cmd == 'del':
-                if param not in userlist:
-                    self.send(self.parsed_line(None, "PRIVMSG", ['Elizacat',  'Not in there.']))
-                    return
-
-                userlist.remove(param)
-
-                if param in npcache:
-                    del npcache[param]
-
-                with open('userlist.txt', 'w') as file:
-                    for l in userlist:
-                        file.write(l)
-                        file.write('\n')
-
-                self.send(self.parsed_line(None, "PRIVMSG", ['Elizacat', 'Done!']))
+                maybe_delete_from_userlist(self, x, param)
 
             elif cmd == 'list':
-                self.send(self.parsed_line(None, "PRIVMSG", ['Elizacat', ' '.join(userlist)]))
+                self.send(self.parsed_line(None, "PRIVMSG", [x, ' '.join(load_users())]))
 
     def do_ping(self, line):
         self.send(self.parsed_line(None, "PONG", line.params))
@@ -196,56 +197,130 @@ class IRC:
                 if p.command in self.commands:
                     self.commands[p.command](p)
 
-def do_poll(irc, lastfm):
-    # FIXME encapsulate
+def load_users():
+    temp = []
+    with open('userlist.txt', 'r') as f:
+        temp = [x.rstrip('\n') for x in f]
+    return temp
+
+def dl_xml_from(url):
+    # Fsck it, we'll do it live, I don't care
+    # I know I'll be murdered unpleasantly by a cat, but I like doing things MY WAY, OK
+    try:
+        last_sock = urlreq.urlopen(url)
+    except Exception as e:
+        if str(e) == "HTTP Error 400: Bad Request":
+            raise Exception("No user with that name was found")
+        else:
+            raise e
+           
+    last_xml = last_sock.read()
+    last_sock.close()
+    last_doc = minidom.parseString(last_xml)
+    return last_doc
+
+def get_np_for(user):
+    info = {}
+    info['track'] = None
+    info['artist'] = None
+    info['album'] = None
+    
+    url = "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user={user}&limit=1&api_key={key}".format(user = user, key = APIKEY)
+    last_doc = dl_xml_from(url)
+    tracks = last_doc.getElementsByTagName("track")
+    if tracks is None:
+       return None # Punt.  Nothing can be done.
+    our_track = tracks[0]
+    try:
+        info['title'] = our_track.getElementsByTagName("name")[0].childNodes[0].data
+        info['artist'] = our_track.getElementsByTagName("artist")[0].childNodes[0].data
+    except:
+        return None  # track name or artist missing, shouldn't scrobble
+    
+    try:
+        info['album'] = our_track.getElementsByTagName("album")[0].childNodes[0].data
+    except:
+        info['album'] = None
+    
+    # makes play count retrieval easier-ish
+    try:
+        info['mbid'] = our_track.getElementsByTagName("mbid")[0].childNodes[0].data
+    except:
+        info['mbid'] = None
+    
+    return info
+
+
+def get_counts_for(track, user):
+    params = ""
+    if track['mbid'] is None:
+        params = "artist={artist}&track={track}".format(artist = track['artist'], track = track['title'])
+    else:
+        params = "mbid={mbid}".format(mbid = track['mbid'])
+    url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&{params}&username={user}&api_key={key}".format(user = user, params = params, key = APIKEY)
+    print(url)
+    last_doc = dl_xml_from(url)
+
+    #try:
+    track_ms = last_doc.getElementsByTagName("duration")[0].childNodes[0].data
+    #except:
+    #    track_ms = 0
+    
+    #try:
+    play_count = last_doc.getElementsByTagName("userplaycount")[0].childNodes[0].data
+    #except:
+    #    play_count = 0
+    
+    duration = '{}:{:02d}'.format(*divmod(int(int(track_ms) / 1000), 60))
+    return (duration, play_count)
+
+def do_poll(irc):
+    global user_changed
+    userlist = load_users()
+    npcache = {}
+    
     for u in userlist:
-        npcache[u] = ((None,)*5, None)
+        npcache[u] = (None,)*5
 
     while True:
+        if user_changed is True:
+            userlist = load_users()
+            npcache = {}
+            
+            for u in userlist:
+                npcache[u] = (None,)*5
+            
+            user_changed = False
+        
         cache = deepcopy(npcache)
         for k, v in cache.items():
-            last, uinstance = v
-            if uinstance is None:
-                uinstance = pylast.User(k, lastfm)
-
+            last = v
             try:
-                sleep(len(userlist)/2)
-                track = uinstance.get_now_playing()
-            except pylast.WSError as e:
+                sleep(1.2)
+                track = get_np_for(k)
+            except Exception as e:
                 if str(e) != "No user with that name was found":
                     raise e
                 
                 userlist.remove(k)
                 del npcache[k]
 
-                print("Error adding user: {}".format(str(e)))
+                print("Error adding user {name}: {exc}".format(name = k, exc = str(e)))
                 continue
 
             if track is None: continue
 
-            artist = track.get_artist()
-            if artist:
-                artist = artist.get_name()
-            else:
-                artist = "[No artist]"
-
-            title = track.get_title()
-
-            album = track.get_album()
-            if album:
-                album = album.get_name()
-            else:
-                album = "None"
-
-            duration = '{}:{:02d}'.format(*divmod(int(track.get_duration() / 1000), 60))
-            try:
-                count = track.get_userplaycount() # NOTE - requires https://github.com/Elizacat/pylast 
-            except:
-                count = 0
+            artist = track['artist']
+            title = track['title']
+            album = track['album']
+            #try:
+            duration, count = get_counts_for(track, k)
+            #except:
+            #    duration = count = 0
 
             np = (artist, title, album, duration, count)
             if last == (None,)*5:
-                npcache[k] = (np, uinstance)
+                npcache[k] = np
                 continue
 
             elif last == np:
@@ -254,22 +329,22 @@ def do_poll(irc, lastfm):
             string = "{} is listening to: {} - {} (album: {}) [{}] | Playcount: {}x".format(k, *np)
             print(string)
             irc.spam_msg(string)
-            npcache[k] = (np, uinstance)
+            npcache[k] = np
 
-def exception_wrapper(irc, lastfm):
+def exception_wrapper(irc):
     while True:
         try:
-            do_poll(irc, lastfm)
+            do_poll(irc)
         except Exception as e:
             irc.spam_msg("last.fm collector crapped itself. Restarting... some NP's may get lost.")
+            print("The reason I crapped all over IRC and it smells real bad is: {exc}".format(exc = e))
             sleep(10)
 
 f = IRC(nick, user, server, port, realname, channels)
 f.connect()
 g1 = spawn(f.dispatch)
 
-network = pylast.LastFMNetwork(api_key = APIKEY, api_secret = SECRET, username = USERNAME, password_hash = PASSWORD)
-g2 = spawn(exception_wrapper, *(f, network))
+g2 = spawn(exception_wrapper, irc=f)
 
 joinall((g1, g2))
 
